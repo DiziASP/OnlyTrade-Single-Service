6733e4559220bdf1e590249fa874eeb69f7991f0
nhelis09 <andyapc09@gmail.com>
2023-12-11 12:16:17 +0700

    modified auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+
+
+$routes->get('/logout', 'LoginController::logout');
+$routes->get('/login', 'LoginController::index');
+$routes->post('/login_action', 'LoginController::login_action');
+$routes->get('register', 'RegisterController::index');
+$routes->post('register_action', 'RegisterController::register_action');
