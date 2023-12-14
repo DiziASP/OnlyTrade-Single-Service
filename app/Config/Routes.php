5ea5cb828a3d968f2cffdd63dad568264ce84d02
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-12-14 15:00:17 +0700

    feat: expose update route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,16 +12,19 @@ use CodeIgniter\Router\RouteCollection;
 // Filter every route in admins except index and show
 $routes->get('/api/admins', 'Admins::index');
 $routes->get('/api/admins/(:num)', 'Admins::show/$1');
+$routes->put('/api/admins/(:num)', 'Admins::update/$1');
 $routes->resource('api/admins', ['filter' => 'jwt']);
 
 // Filter every route in barang except index and show
 $routes->get('/api/barang', 'Barang::index');
 $routes->get('/api/barang/(:num)', 'Barang::show/$1');
+$routes->put('/api/barang/(:num)', 'Barang::update/$1');
 $routes->resource('/api/barang', ['filter' => 'jwt']);
 
 // Filter every route in perusahaan except index and show
 $routes->get('/api/perusahaan', 'Perusahaan::index');
 $routes->get('/api/perusahaan/(:num)', 'Perusahaan::show/$1');
+$routes->put('/api/perusahaan/(:num)', 'Perusahaan::update/$1');
 $routes->resource('api/perusahaan', ['filter' => 'jwt']);
 
 // Auth Routes
