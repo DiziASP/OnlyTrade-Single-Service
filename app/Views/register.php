d182e9753d81e960f6649a28ca698d76604f10eb
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-12-15 21:51:27 +0700

    feat: fix bug register

--- app/Views/register.php
+++ app/Views/register.php
@@ -45,7 +45,7 @@
     </nav>
     <div class="container">
         <h2 class="mb-4 text-center">Register</h2>
-        <form action="/register_action" method="post">
+        <form action="/api/auth/register" method="post">
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" required>
@@ -59,6 +59,11 @@
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password" name="password" placeholder="Create your password" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Create your password" required>
             </div>
             <div class="d-flex justify-content-center">
                 <button type="submit" class="btn btn-red">Register</button>
